f76c3368dcea17a3d2367dbd1be5b92f4b2630d2
Nivedita AP <niveditaprasannaraj@gmail.com>
2022-06-26 21:12:43 +0530

    updated logic

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
 const numberOfNotes = document.querySelectorAll(".noOfNotes");
+const hideCashGiven = document.querySelector(".hide-cashGiven");
+const hidetable = document.querySelector(".hide-table");
+const nextbtn = document.querySelector("#nextbtn");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+hideCashGiven.style.display = "none";
+hidetable.style.display = "none";
+
+nextbtn.addEventListener("click", function(){
+    hideCashGiven.style.display = "block";
+    hidetable.style.display = "block";
+} );
+
 
 checkButton.addEventListener("click", function validateBillAndCashAmount(){
     hideMessage();
     if(billAmount.value > 0){
-        if(cashGiven.value >= billAmount.value){
+        if(Number(cashGiven.value) >= Number(billAmount.value)){
             const amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
         }else{
